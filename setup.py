29037c75fde3d79c7efb3c46ac24716b420632b0
yashjhota <jhotayash@gmail.com>
2025-03-16 11:45:15 +0530

    Structured defined

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Yash Jain",
+    author_email="jhotayash@gmail.com",
+    packages=find_packages()
+)
